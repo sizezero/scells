5e79d23b2b974c99ae5aad842c784926c20a79cd
Robert Kleemann <robert@kleemann.org>
2014-11-02 11:23:19 -0800

    added simple cell renderer

---
+++ src/main/scala/org/kleemann/scells/Model.scala
@@ -0,0 +1,13 @@
+package org.kleemann.scells
+
+class Model(val height: Int, val width: Int) {
+  
+  case class Cell(row: Int, column: Int)
+  
+  // It looks like the scala libaries have changed since this example was written
+  //val cells = new Array[Array[Cell]](height)(width)
+  val cells = Array.ofDim[Cell](height, width)
+  
+  for (i <- 0 until height; j <- 0 until width)
+    cells(i)(j) = new Cell(i, j)
+}
--- src/main/scala/org/kleemann/scells/Spreadsheet.scala
+++ src/main/scala/org/kleemann/scells/Spreadsheet.scala
@@ -3,12 +3,32 @@ package org.kleemann.scells
 import swing._
 
 class Spreadsheet(val height: Int, val width: Int) extends ScrollPane {
+
+  val cellModel = new Model(height, width)
+  import cellModel._
+
   val table = new Table(height, width) {
     rowHeight = 25
     autoResizeMode = Table.AutoResizeMode.Off
     showGrid = true
     gridColor = new java.awt.Color(150, 150, 150)
+    
+    override def rendererComponent(isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component =
+      // How often does this get called? Are we creating lots of these objects?
+      if (hasFocus) new TextField(userData(row, column))
+      else
+        new Label(cells(row)(column).toString) {
+          xAlignment = Alignment.Right
+        }
+    
+    def userData(row: Int, column: Int): String = {
+      // syntax calls the apply method on the current Table instance.
+      // This returns a value of type "Any" which can be pattern matched against
+      val v = this(row, column)
+      if (v == null) "" else v.toString
+    }
   }
+  
   val rowHeader =
     new ListView((0 until height) map (_.toString)) {
       fixedCellWidth = 30
